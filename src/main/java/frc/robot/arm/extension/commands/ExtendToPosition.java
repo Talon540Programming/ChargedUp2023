106dfb54b2e85f3c4432d9ca5234429f4dcef31c
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-01-11 13:27:44 -0500

    optimized setting extension percent

--- src/main/java/frc/robot/arm/extension/commands/ExtendToPosition.java
+++ src/main/java/frc/robot/arm/extension/commands/ExtendToPosition.java
@@ -3,13 +3,15 @@ package frc.robot.arm.extension.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.arm.extension.ArmExtensionBase;
 import frc.robot.arm.extension.ArmExtensionBase.ArmExtensionPosition;
+import frc.robot.constants.Constants;
 
-public abstract class ExtendToPosition extends CommandBase {
+public class ExtendToPosition extends CommandBase {
   private final ArmExtensionBase extensionBase;
   private final ArmExtensionPosition targetPosition;
 
   public ExtendToPosition(ArmExtensionBase armExtensionBase, ArmExtensionPosition targetPosition) {
     addRequirements(armExtensionBase);
+
     this.extensionBase = armExtensionBase;
     if (targetPosition == ArmExtensionPosition.kUnknown)
       throw new IllegalArgumentException("Cannot extend to an unknown position");
@@ -18,9 +20,9 @@ public abstract class ExtendToPosition extends CommandBase {
 
   @Override
   public void execute() {
-    ArmExtensionBase.ArmExtensionPosition lastArmPosition = extensionBase.getLastPosition();
+    ArmExtensionBase.ArmExtensionPosition lastKnownArmPosition = extensionBase.getLastPosition();
 
-    if (lastArmPosition == ArmExtensionPosition.kUnknown) {
+    if (lastKnownArmPosition == ArmExtensionPosition.kUnknown) {
       // The position of the arm is unknown, move forward until it is known, hopefully before it
       // hits the absolute max.
       extensionBase.setExtensionPercent(0.5);
@@ -28,15 +30,7 @@ public abstract class ExtendToPosition extends CommandBase {
     }
 
     // Get the position of the target relative to the last position
-    int positionRelativeLocation = targetPosition.compareTo(lastArmPosition);
-
-    if (positionRelativeLocation > 0) {
-      // Target is ahead of the current position, release the winch
-      extensionBase.setExtensionPercent(0.25);
-    } else if (positionRelativeLocation < 0) {
-      // Target is behind the current position, pull in the winch
-      extensionBase.setExtensionPercent(-0.25);
-    }
+    extensionBase.setExtensionPercent(Math.signum(targetPosition.compareTo(lastKnownArmPosition)) * Constants.kExtensionRate);
   }
 
   @Override
