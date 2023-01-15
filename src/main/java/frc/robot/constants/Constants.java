9b7c9e723bf91feb6cf4305614ab93f0848c172b
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-01-15 11:27:47 -0500

    rename constants

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -10,11 +10,14 @@ public class Constants {
       public static final double kMaxRotationalVelocity = 0; // TODO
       public static final double kMaxRotationalAcceleration = 0; // TODO
 
+      // Gearbox * Sprocket
+      public static final double kGearRatio = 0; // TODO
+
       public static class ControlValues {
         public static double kP = 0; // TODO
         public static double kI = 0; // TODO
         public static double kD = 0; // TODO
-        public static double kG = 0; // TODO
+        public static double kS = 0; // TODO
         public static double kV = 0; // TODO
       }
     }
