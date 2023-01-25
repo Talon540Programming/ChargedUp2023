e6e56fb166ca81b1570f6e89e77a196191c6f5c7
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-01-25 11:21:47 -0500

    Add higher level metadata

--- src/main/java/frc/lib/logging/LoggerUtil.java
+++ src/main/java/frc/lib/logging/LoggerUtil.java
@@ -1,6 +1,8 @@
 package frc.lib.logging;
 
+import edu.wpi.first.wpilibj.RobotBase;
 import frc.robot.BuildConstants;
+import frc.robot.constants.Constants;
 import org.littletonrobotics.junction.Logger;
 
 /** Utilities used by the AdvantageKit Logger. */
@@ -12,6 +14,8 @@ public class LoggerUtil {
    */
   public static void initializeLoggerMetadata(Logger logger) {
     // Record metadata from generated state file.
+    logger.recordMetadata("Robot", Constants.getRobotType().name());
+    logger.recordMetadata("RuntimeEnvironment", RobotBase.getRuntimeType().name());
     logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME);
     logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
     logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
