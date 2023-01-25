f19786968a83481db1e4c69107c2749481be2a5e
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-01-25 04:14:30 -0500

    Update DriveBase.java

--- src/main/java/frc/robot/drivetrain/DriveBase.java
+++ src/main/java/frc/robot/drivetrain/DriveBase.java
@@ -6,7 +6,6 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.drive.RobotDriveBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.Constants.Drivetrain;
