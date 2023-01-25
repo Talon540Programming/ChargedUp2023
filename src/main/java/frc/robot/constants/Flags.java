1fe445b5e88772c1c1108a41e083dfbdddc17cb1
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-01-25 10:15:36 -0500

    Remove useless flag docs

--- src/main/java/frc/robot/constants/Flags.java
+++ src/main/java/frc/robot/constants/Flags.java
@@ -2,11 +2,9 @@ package frc.robot.constants;
 
 public final class Flags {
   public enum RobotMode {
-    /** Running on the proto robot. */
     PROTO,
-    /** Running on the comp robot. */
     COMP,
-    /** Replaying from a log file. */
+    SIM,
     REPLAY
   }
 
