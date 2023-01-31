82ebefc438de00d58673cc9296ed2031e5a5e76e
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-01-31 10:14:23 -0500

    Update metadata keys

--- src/main/java/frc/lib/logging/LoggerUtil.java
+++ src/main/java/frc/lib/logging/LoggerUtil.java
@@ -22,19 +22,19 @@ public class LoggerUtil {
    */
   public static void initializeLoggerMetadata(Logger logger) {
     // Record metadata from generated state file.
-    logger.recordMetadata("Robot", Constants.getRobotType().name());
-    logger.recordMetadata("RuntimeEnvironment", RobotBase.getRuntimeType().name());
-    logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME);
-    logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
-    logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
-    logger.recordMetadata("GitDate", BuildConstants.GIT_DATE);
-    logger.recordMetadata("GitBranch", BuildConstants.GIT_BRANCH);
+    logger.recordMetadata("ROBOT_NAME", Constants.getRobotType().toString());
+    logger.recordMetadata("RUNTIME_ENVIRONMENT", RobotBase.getRuntimeType().toString());
+    logger.recordMetadata("PROJECT_NAME", BuildConstants.MAVEN_NAME);
+    logger.recordMetadata("BUILD_DATE", BuildConstants.BUILD_DATE);
+    logger.recordMetadata("GIT_SHA", BuildConstants.GIT_SHA);
+    logger.recordMetadata("GIT_DATE", BuildConstants.GIT_DATE);
+    logger.recordMetadata("GIT_BRANCH", BuildConstants.GIT_BRANCH);
 
     // Set the current GIT state of the robot (helps manage the logs that are saved).
     switch (BuildConstants.DIRTY) {
-      case 0 -> logger.recordMetadata("GitDirty", "All changes committed");
-      case 1 -> logger.recordMetadata("GitDirty", "Uncommitted changes");
-      default -> logger.recordMetadata("GitDirty", "Unknown");
+      case 0 -> logger.recordMetadata("GIT_STATUS", "All changes committed");
+      case 1 -> logger.recordMetadata("GIT_STATUS", "Uncommitted changes");
+      default -> logger.recordMetadata("GIT_STATUS", "Unknown");
     }
   }
 
