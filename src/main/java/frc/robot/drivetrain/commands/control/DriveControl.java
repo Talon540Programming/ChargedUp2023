6f7d6d37f8755c969101fa79e07ee29c58fd2a0d
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-02-06 00:03:04 -0500

    Add option to switch between tank and arcade drive

--- src/main/java/frc/robot/drivetrain/commands/control/DriveControl.java
+++ src/main/java/frc/robot/drivetrain/commands/control/DriveControl.java
@@ -9,17 +9,28 @@ import frc.robot.drivetrain.DriveBase;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 public abstract class DriveControl extends CommandBase {
+  public enum DriveMode {
+    Differential,
+    Arcade
+  }
+
   protected double kLeftPercent, kRightPercent;
   private final DriveBase m_driveBase;
 
   private static final LoggedDashboardChooser<Double> m_speedLimiter =
       new LoggedDashboardChooser<>("Drive Speed Limit");
 
+  private static final LoggedDashboardChooser<DriveMode> m_driveMode =
+          new LoggedDashboardChooser<>("Drive Mode");
+
   static {
     m_speedLimiter.addDefaultOption("Default (100%)", 1.0);
     m_speedLimiter.addOption("Fast (70%)", 0.7);
     m_speedLimiter.addOption("Medium (30%)", 0.3);
     m_speedLimiter.addOption("Slow (15%)", 0.15);
+
+    m_driveMode.addDefaultOption("Differential (Tank) Drive", DriveMode.Differential);
+    m_driveMode.addOption("Arcade Drive", DriveMode.Arcade);
   }
 
   protected DriveControl(DriveBase driveBase) {
@@ -33,6 +44,14 @@ public abstract class DriveControl extends CommandBase {
     kLeftPercent *= m_speedLimiter.get();
     kRightPercent *= m_speedLimiter.get();
 
-    this.m_driveBase.tankDrivePercent(kLeftPercent, kRightPercent);
+    switch (m_driveMode.get()) {
+      case Differential -> {
+        this.m_driveBase.tankDrivePercent(kLeftPercent, kRightPercent);
+      }
+      case Arcade -> {
+        this.m_driveBase.arcadeDrivePercent(kLeftPercent, kRightPercent);
+      }
+    }
+
   }
 }
