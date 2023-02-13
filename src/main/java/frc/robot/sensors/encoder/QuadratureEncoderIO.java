3ce3a8f4c1bc7120677a531463fa65640fa3954b
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-02-13 02:36:44 -0500

    Add Encoder

--- src/main/java/frc/robot/sensors/encoder/QuadratureEncoderIO.java
+++ src/main/java/frc/robot/sensors/encoder/QuadratureEncoderIO.java
@@ -1,15 +1,20 @@
 package frc.robot.sensors.encoder;
 
+import edu.wpi.first.wpilibj.interfaces.Gyro;
 import frc.lib.logging.LoggedIO;
 
 public interface QuadratureEncoderIO
     extends LoggedIO<QuadratureEncoderIO.QuadratureEncoderIOInputs> {
   public class QuadratureEncoderIOInputs {
     public double AbsolutePositionRad;
+    public double PositionRad;
     public double VelocityRadPerSecond;
   }
 
   @Override
   default void updateInputs(QuadratureEncoderIOInputs inputs) {}
-  ;
+
+  default public void setPosition(double position) {}
+
+  default public void resetToAbsolute() {}
 }
