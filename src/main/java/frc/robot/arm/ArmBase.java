4102d1fd86461ed6f41cffdcf24d140515aafb21
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-02-15 09:50:47 -0500

    Formatting fixes

--- src/main/java/frc/robot/arm/ArmBase.java
+++ src/main/java/frc/robot/arm/ArmBase.java
@@ -9,14 +9,14 @@ import org.littletonrobotics.junction.Logger;
 
 public class ArmBase extends SubsystemBase {
   private final ArmExtensionIO m_armExtensionIO;
-  public final ArmExtensionIOInputsAutoLogged m_armExtensionInputs = new ArmExtensionIOInputsAutoLogged();
+  public final ArmExtensionIOInputsAutoLogged m_armExtensionInputs =
+      new ArmExtensionIOInputsAutoLogged();
 
   private final ArmRotationIO m_armRotationIO;
-  public final ArmRotationIOInputsAutoLogged m_armRotationInputs = new ArmRotationIOInputsAutoLogged();
+  public final ArmRotationIOInputsAutoLogged m_armRotationInputs =
+      new ArmRotationIOInputsAutoLogged();
 
-  public ArmBase(
-      ArmExtensionIO extensionIO,
-      ArmRotationIO rotationIO) {
+  public ArmBase(ArmExtensionIO extensionIO, ArmRotationIO rotationIO) {
     this.m_armExtensionIO = extensionIO;
     this.m_armRotationIO = rotationIO;
   }
--- src/main/java/frc/robot/arm/rotation/ArmRotationIOFalcon.java
+++ src/main/java/frc/robot/arm/rotation/ArmRotationIOFalcon.java
@@ -9,36 +9,36 @@ import frc.robot.constants.Constants;
 import org.talon540.hardware.CANDeviceConfig;
 
 public class ArmRotationIOFalcon implements ArmRotationIO {
-    private final WPI_TalonFX m_leader, m_follower;
+  private final WPI_TalonFX m_leader, m_follower;
 
-    public ArmRotationIOFalcon(CANDeviceConfig leader, CANDeviceConfig follower) {
-        this.m_leader = new WPI_TalonFX(leader.id, leader.controller);
-        this.m_follower = new WPI_TalonFX(follower.id, follower.controller);
+  public ArmRotationIOFalcon(CANDeviceConfig leader, CANDeviceConfig follower) {
+    this.m_leader = new WPI_TalonFX(leader.id, leader.controller);
+    this.m_follower = new WPI_TalonFX(follower.id, follower.controller);
 
-        TalonFXConfiguration config = new TalonFXConfiguration();
-        config.voltageCompSaturation = 12.0;
-        config.statorCurrLimit = new StatorCurrentLimitConfiguration(true, 40, 0, 0);
+    TalonFXConfiguration config = new TalonFXConfiguration();
+    config.voltageCompSaturation = 12.0;
+    config.statorCurrLimit = new StatorCurrentLimitConfiguration(true, 40, 0, 0);
 
-        m_leader.configAllSettings(config);
-        m_follower.configAllSettings(config);
+    m_leader.configAllSettings(config);
+    m_follower.configAllSettings(config);
 
-        m_leader.setNeutralMode(NeutralMode.Brake);
-        m_follower.setNeutralMode(NeutralMode.Brake);
+    m_leader.setNeutralMode(NeutralMode.Brake);
+    m_follower.setNeutralMode(NeutralMode.Brake);
 
-        m_follower.follow(m_leader);
+    m_follower.follow(m_leader);
 
-        m_follower.setInverted(Constants.Arm.kRotationInverted);
-        m_leader.setInverted(InvertType.FollowMaster);
-    }
+    m_follower.setInverted(Constants.Arm.kRotationInverted);
+    m_leader.setInverted(InvertType.FollowMaster);
+  }
 
-    @Override
-    public void updateInputs(ArmRotationIOInputs inputs) {
-        inputs.SupplyCurrentAmps = (m_leader.getSupplyCurrent() + m_follower.getSupplyCurrent()) / 2;
-        inputs.StatorCurrentAmps = (m_leader.getStatorCurrent() + m_follower.getStatorCurrent()) / 2;
-    }
+  @Override
+  public void updateInputs(ArmRotationIOInputs inputs) {
+    inputs.SupplyCurrentAmps = (m_leader.getSupplyCurrent() + m_follower.getSupplyCurrent()) / 2;
+    inputs.StatorCurrentAmps = (m_leader.getStatorCurrent() + m_follower.getStatorCurrent()) / 2;
+  }
 
-    @Override
-    public void setVoltage(double voltage) {
-        m_leader.setVoltage(voltage);
-    }
+  @Override
+  public void setVoltage(double voltage) {
+    m_leader.setVoltage(voltage);
+  }
 }
