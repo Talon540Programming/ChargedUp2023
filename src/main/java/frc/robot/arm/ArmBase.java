771983ee5160e620f50f07aea9f118d3e3982462
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-02-15 00:42:03 -0500

    Removed unknown sensors and related stuff

--- src/main/java/frc/robot/arm/ArmBase.java
+++ src/main/java/frc/robot/arm/ArmBase.java
@@ -1,38 +1,24 @@
 package frc.robot.arm;
 
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.arm.extension.ArmExtensionIO;
 import frc.robot.arm.extension.ArmExtensionIOInputsAutoLogged;
 import frc.robot.arm.rotation.ArmRotationIO;
 import frc.robot.arm.rotation.ArmRotationIOInputsAutoLogged;
-import frc.robot.sensors.BeamBreakSensor;
 import org.littletonrobotics.junction.Logger;
 
 public class ArmBase extends SubsystemBase {
   private final ArmExtensionIO m_armExtensionIO;
-  public final ArmExtensionIOInputsAutoLogged m_armExtensionInputs =
-      new ArmExtensionIOInputsAutoLogged();
+  public final ArmExtensionIOInputsAutoLogged m_armExtensionInputs = new ArmExtensionIOInputsAutoLogged();
 
   private final ArmRotationIO m_armRotationIO;
-  public final ArmRotationIOInputsAutoLogged m_armRotationInputs =
-      new ArmRotationIOInputsAutoLogged();
-
-  private final BeamBreakSensor m_forwardBeamBreak, m_reverseBeamBreak;
-  private final DigitalInput m_homeSensor;
+  public final ArmRotationIOInputsAutoLogged m_armRotationInputs = new ArmRotationIOInputsAutoLogged();
 
   public ArmBase(
       ArmExtensionIO extensionIO,
-      ArmRotationIO rotationIO,
-      int forwardPort,
-      int reversePort,
-      int homeSensorPort) {
+      ArmRotationIO rotationIO) {
     this.m_armExtensionIO = extensionIO;
     this.m_armRotationIO = rotationIO;
-
-    this.m_forwardBeamBreak = new BeamBreakSensor(forwardPort);
-    this.m_reverseBeamBreak = new BeamBreakSensor(reversePort);
-    this.m_homeSensor = new DigitalInput(homeSensorPort);
   }
 
   @Override
@@ -42,21 +28,5 @@ public class ArmBase extends SubsystemBase {
 
     m_armRotationIO.updateInputs(m_armRotationInputs);
     Logger.getInstance().processInputs("Arm/Rotation", m_armRotationInputs);
-
-    Logger.getInstance().recordOutput("Arm/Forward Beam Break", atForwardLimit());
-    Logger.getInstance().recordOutput("Arm/Reverse Beam Break", atRearLimit());
-    Logger.getInstance().recordOutput("Arm/HOME HALL Sensor", extensionAtHome());
-  }
-
-  public boolean extensionAtHome() {
-    return m_homeSensor.get();
-  }
-
-  public boolean atForwardLimit() {
-    return m_forwardBeamBreak.isBeamBroken();
-  }
-
-  public boolean atRearLimit() {
-    return m_reverseBeamBreak.isBeamBroken();
   }
 }
