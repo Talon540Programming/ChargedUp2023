d252b5151eb9c0f1851d254a05fabaefb23d375c
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-02-16 20:19:07 -0500

    Update ArmState.java

--- src/main/java/frc/lib/arm/ArmState.java
+++ src/main/java/frc/lib/arm/ArmState.java
@@ -1,25 +1,12 @@
 package frc.lib.arm;
 
-import edu.wpi.first.math.geometry.Rotation2d;
+import org.littletonrobotics.junction.LogTable;
+import org.littletonrobotics.junction.inputs.LoggableInputs;
 
 /** An object that can be used to track or represent the state of, or desired state of the arm. */
-public class ArmState {
-  public Rotation2d angle;
-  public double extensionLengthMeters;
-
-  /**
-   * Create an ArmState object.
-   *
-   * @param angle Angle of the arm. This angle should be similar to the Unit circle where 0 means
-   *     the arm is perpendicular to the robot's fulcrum and parallel to the floor, facing towards
-   *     the front of the robot. The angle is CCW positive.
-   * @param extensionLengthMeters The length that the arm is extended to beyond the fully retracted state at without the grabber or any other
-   *     attachment at the end in meters.
-   */
-  public ArmState(Rotation2d angle, double extensionLengthMeters) {
-    this.angle = angle;
-    this.extensionLengthMeters = extensionLengthMeters;
-  }
+public class ArmState implements LoggableInputs {
+  public double AngleRadians;
+  public double ExtensionLengthMeters;
 
   /**
    * Create an ArmState object.
@@ -27,10 +14,24 @@ public class ArmState {
    * @param angleRadians Angle of the Arm in radians. This angle should be similar to the Unit
    *     circle where 0 means the arm is perpendicular to the robot's fulcrum and parallel to the
    *     floor, facing towards the front of the robot. The angle is CCW positive.
-   * @param extensionLengthMeters The length that the arm is extended to beyond the fully retracted state at without the grabber or any other
-   *    *     attachment at the end in meters.
+   * @param extensionLengthMeters The length that the arm is extended to beyond the fully retracted
+   *     state at without the grabber or any other * attachment at the end in meters.
    */
   public ArmState(double angleRadians, double extensionLengthMeters) {
-    this(Rotation2d.fromRadians(angleRadians), extensionLengthMeters);
+    this.AngleRadians = angleRadians;
+    this.ExtensionLengthMeters = extensionLengthMeters;
+  }
+
+  @Override
+  public void toLog(LogTable table) {
+    table.put("AngleRadians", AngleRadians);
+    table.put("ExtensionLengthMeters", ExtensionLengthMeters);
+  }
+
+  @Override
+  public void fromLog(LogTable table) {
+    AngleRadians = table.getDouble("AngleRadians", AngleRadians);
+    ExtensionLengthMeters = table.getDouble("ExtensionLengthMeters", ExtensionLengthMeters);
+  }
   }
 }
