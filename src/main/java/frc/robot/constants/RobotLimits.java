f420ba87604e817a8c8db7a152bf700764a93fe6
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-02-18 13:50:14 -0500

    Added control values for arm

--- src/main/java/frc/robot/constants/RobotLimits.java
+++ src/main/java/frc/robot/constants/RobotLimits.java
@@ -4,13 +4,14 @@ import edu.wpi.first.math.util.Units;
 
 public class RobotLimits {
   public static final double kMaxExtensionHorizontalInches = 48; // 48" Extension Limit
-  public static final double kMaxExtensionHorizontalMeters =
-      Units.inchesToMeters(kMaxExtensionHorizontalInches);
+  public static final double kMaxExtensionHorizontalMeters = Units.inchesToMeters(kMaxExtensionHorizontalInches);
 
   public static final double kMaxExtensionVerticalInches = 78; // 6'6" Max Height Limit
-  public static final double kMaxExtensionVerticalMeters =
-      Units.inchesToMeters(kMaxExtensionVerticalInches);
+  public static final double kMaxExtensionVerticalMeters = Units.inchesToMeters(kMaxExtensionVerticalInches);
 
   public static final double kForwardLimitRadians = 2.0 * Math.PI; // TODO
   public static final double kRearLimit = Math.PI; // TODO
+
+  public static final double kMaxArmVelocityRadPerSecond = 0; // TODO
+  public static final double kMaxArmAccelerationRadPerSecondSquared = 0; // TODO
 }
