44acb4bd9c946fb8fc81ad8d7a977e8f25a8dcd5
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-02-21 10:33:08 -0500

    Update PhotonCamera.java

--- src/main/java/frc/lib/vision/PhotonCamera.java
+++ src/main/java/frc/lib/vision/PhotonCamera.java
@@ -1,24 +1,24 @@
 package frc.lib.vision;
 
 
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
 
 import java.util.function.Supplier;
 
-public class PhotonCamera {
-    private final org.photonvision.PhotonCamera m_camera;
+public class PhotonCamera extends org.photonvision.PhotonCamera {
+    private final Supplier<Transform3d> kRobotToCamera;
 
-    private final Supplier<Translation3d> kRobotToCamera;
-
-    public PhotonCamera(String subtableName, Supplier<Translation3d> robotToCameraSupplier) {
-        m_camera = new org.photonvision.PhotonCamera(subtableName);
-        kRobotToCamera = robotToCameraSupplier;
+    public PhotonCamera(String subtableName, Supplier<Transform3d> robotToCameraSupplier) {
+        super(subtableName);
+            kRobotToCamera = robotToCameraSupplier;
     }
 
-    public PhotonCamera(String subtableName, Translation3d robotToCamera) {
+    public PhotonCamera(String subtableName, Transform3d robotToCamera) {
         this(subtableName, () -> robotToCamera);
     }
 
-
-
+    public Transform3d getRobotToCamera() {
+        return kRobotToCamera.get();
+    }
 }
