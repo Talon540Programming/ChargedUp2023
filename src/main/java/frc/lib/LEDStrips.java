48ed15ab5868f7b6c14a164293c858bbd0c2fc99
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-02-27 12:36:10 -0500

    Add LED Strip Hardware Abstraction

---
+++ src/main/java/frc/lib/LEDStrips.java
@@ -0,0 +1,40 @@
+package frc.lib;
+
+
+import edu.wpi.first.wpilibj.PWM;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.util.Color8Bit;
+
+public class LEDStrips {
+    private final PWM redChannel;
+    private final PWM greenChannel;
+    private final PWM blueChannel;
+
+    public LEDStrips(int redPin, int greenPin, int bluePin) {
+        this.redChannel = new PWM(redPin);
+        this.greenChannel = new PWM(greenPin);
+        this.blueChannel = new PWM(bluePin);
+    }
+
+    public void setRedValue(int value) {
+        redChannel.setRaw(value);
+    }
+
+    public void setGreenValue(int value) {
+        greenChannel.setRaw(value);
+    }
+
+    public void setBlueValue(int value) {
+        blueChannel.setRaw(value);
+    }
+
+    public void setColor(Color8Bit color) {
+        setRedValue(color.red);
+        setGreenValue(color.green);
+        setBlueValue(color.blue);
+    }
+
+    public void setColor(Color color) {
+        setColor(new Color8Bit(color));
+    }
+}
