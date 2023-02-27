b8bab7edaf0441db9023818e74225284f4298698
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-02-27 17:50:37 -0500

    add pins

--- Fester/Fester.ino
+++ Fester/Fester.ino
@@ -6,17 +6,17 @@
 // Configure LED Matrix
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
 #define MAX_DEVICES 4
-#define CS_PIN 0 // TODO
+#define CS_PIN 3 // TODO
 
 MD_Parola m_display = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 
 // Messages to Scroll
-const char *message = "GAP BOT   AGENCY   BOT IN THREE WEEKS";
+const char *message = "GAP BOT   AGENCY   BOT IN THREE WEEKS"; // TODO ADD AMONG US EMOJI, ADD SALUTE
 
 void setup() {
   // Setup the LED Display
   m_display.begin();
-  m_display.displayScroll(message, PA_CENTER, PA_SCROLL_RIGHT, 100);
+  m_display.displayScroll(message, PA_CENTER, PA_SCROLL_LEFT, 50);
 }
 
 void loop() {
