2b6824c68c0c7820f7949927ba7aa13f34272647
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-03-05 23:39:24 -0500

    Renamed Stabilize auto

--- src/main/java/frc/robot/autos/StabilizeOnly.java
+++ src/main/java/frc/robot/autos/StabilizeOnly.java
@@ -10,14 +10,14 @@ import frc.robot.drivetrain.commands.StabilizeRobot;
  * Represents an Auto Routine used to drive forward at a given percent until the robot begins to
  * incline or decline, then it will stabilize itself then end.
  */
-public class StabilizeOnlyAuto extends SequentialCommandGroup {
+public class StabilizeOnly extends SequentialCommandGroup {
   /**
-   * Create a StabilizeOnlyAuto Auto.
+   * Create a StabilizeOnly Auto.
    *
    * @param speed the speed to drive at while seeking.
    * @param drivebase drivetrain subsystem.
    */
-  public StabilizeOnlyAuto(double speed, DriveBase drivebase) {
+  public StabilizeOnly(double speed, DriveBase drivebase) {
     super(
         Commands.deadline(
             Commands.waitUntil(() -> !drivebase.isLevel()),
