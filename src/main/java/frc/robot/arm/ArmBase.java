f3c8dafff97d908f91f0d51d2256b1da4e4887d1
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-03-15 16:58:46 -0400

    Plot Arm as a Mech2d object

--- src/main/java/frc/robot/arm/ArmBase.java
+++ src/main/java/frc/robot/arm/ArmBase.java
@@ -11,10 +11,8 @@ import edu.wpi.first.wpilibj.util.Color8Bit;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.arm.extension.ArmExtensionIO;
 import frc.robot.arm.extension.ArmExtensionIOInputsAutoLogged;
-import frc.robot.arm.extension.Winch;
 import frc.robot.arm.rotation.ArmRotationIO;
 import frc.robot.arm.rotation.ArmRotationIOInputsAutoLogged;
-import frc.robot.constants.Constants;
 import frc.robot.constants.RobotDimensions;
 import org.littletonrobotics.junction.Logger;
 
@@ -27,38 +25,28 @@ public class ArmBase extends SubsystemBase {
   public final ArmRotationIOInputsAutoLogged m_armRotationInputs =
       new ArmRotationIOInputsAutoLogged();
 
-  private final Winch m_extensionWinch;
+  Mechanism2d m_mech = new Mechanism2d(Units.inchesToMeters(200), Units.inchesToMeters(200));
 
-  Mechanism2d m_mech = new Mechanism2d(
-    Units.inchesToMeters(96), Units.inchesToMeters(78));
+  MechanismRoot2d m_mechFulcrum =
+      m_mech.getRoot(
+          "ArmFulcrum", Units.inchesToMeters(48), RobotDimensions.Arm.kFulcrumHeightMeters);
 
-  MechanismRoot2d m_mechFulcrum = m_mech.getRoot(
-    "ArmFulcrum", Units.inchesToMeters(48), RobotDimensions.Arm.kFulcrumHeightMeters);
-
-
-  MechanismLigament2d m_mechUpright = 
-  m_mechFulcrum.append(new MechanismLigament2d("ArmTower", RobotDimensions.Arm.kFulcrumHeightMeters, -90));
+  MechanismLigament2d m_mechUpright =
+      m_mechFulcrum.append(
+          new MechanismLigament2d("ArmTower", RobotDimensions.Arm.kFulcrumHeightMeters, -90));
 
   private final MechanismLigament2d m_mechArm =
       m_mechFulcrum.append(
-        new MechanismLigament2d(
-            "Arm",
-            25 / 39.37,
-            Units.radiansToDegrees(m_armRotationInputs.AbsoluteArmPositionRad),
-            6,
-            new Color8Bit(Color.kYellow)));
+          new MechanismLigament2d(
+              "Arm",
+              Units.inchesToMeters(25),
+              Units.radiansToDegrees(m_armRotationInputs.AbsoluteArmPositionRad),
+              6,
+              new Color8Bit(Color.kYellow)));
 
   public ArmBase(ArmExtensionIO extensionIO, ArmRotationIO rotationIO) {
     this.m_armExtensionIO = extensionIO;
     this.m_armRotationIO = rotationIO;
-
-    this.m_extensionWinch =
-        new Winch(
-            Constants.Arm.kExtensionWinchRadiusMeters,
-            Constants.Arm.kExtensionCableLengthMeters,
-            Constants.Arm.kExtensionCableDiameterMeters,
-            Constants.Arm.kNumberOfWrapsPerRadiusIncrease,
-            Constants.Arm.kInitialWrapsAtBoot);
 
     SmartDashboard.putData("Arm Simulation", m_mech);
     m_mechUpright.setColor(new Color8Bit(Color.kAqua));
@@ -69,13 +57,11 @@ public class ArmBase extends SubsystemBase {
     m_armExtensionIO.updateInputs(m_armExtensionInputs);
     Logger.getInstance().processInputs("Arm/Extension", m_armExtensionInputs);
 
-    Logger.getInstance()
-        .recordOutput("Arm/Extension/Winch/Distance", getExtensionDistanceTraveled());
-
     m_armRotationIO.updateInputs(m_armRotationInputs);
     Logger.getInstance().processInputs("Arm/Rotation", m_armRotationInputs);
 
     m_mechArm.setAngle(Rotation2d.fromRadians(m_armRotationInputs.AbsoluteArmPositionRad));
+    m_mechArm.setLength(m_armExtensionInputs.DistanceTraveledMeters);
 
     // Log the target state
     Logger.getInstance()
@@ -106,6 +92,7 @@ public class ArmBase extends SubsystemBase {
    * @return distance traveled by the arm.
    */
   public double getExtensionDistanceTraveled() {
-    return m_extensionWinch.getDistanceTraveled(m_armExtensionInputs.DistanceTraveledMeters);
+    // return m_extensionWinch.getDistanceTraveled(m_armExtensionInputs.DistanceTraveledMeters);
+    return 0.0;
   }
 }
