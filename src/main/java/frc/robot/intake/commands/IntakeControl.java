f14eacb3b5d00003e7e9fdf9bfe538ac457a5b21
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-03-20 23:45:47 -0400

    Create IntakeControl.java

---
+++ src/main/java/frc/robot/intake/commands/IntakeControl.java
@@ -0,0 +1,17 @@
+package frc.robot.intake.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.intake.IntakeBase;
+import frc.robot.oi.OperatorInterface;
+
+public class IntakeControl extends CommandBase {
+    private final IntakeBase m_intakeBase;
+    private final OperatorInterface m_operatorInterface;
+
+    public IntakeControl(IntakeBase intakeBase, OperatorInterface operatorInterface) {
+        this.m_intakeBase = intakeBase;
+        this.m_operatorInterface = operatorInterface;
+
+        addRequirements(intakeBase);
+    }
+}
