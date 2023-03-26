13b05df856a8c601ccca621cf4114b5724c3f5ed
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-03-25 22:30:01 -0400

    Update constants

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -13,7 +13,7 @@ public final class Constants {
    */
   public static final boolean kAdvancedLoggingEnabled = true;
 
-  private static RobotType kRobotType = RobotType.ROBOT_SIMBOT;
+  private static RobotType kRobotType = RobotType.ROBOT_2023C;
   public static final double loopPeriodSecs = 0.02;
 
   public enum RobotMode {
@@ -70,7 +70,7 @@ public final class Constants {
 
     public static final NeutralMode kDrivetrainDefaultNeutralMode = NeutralMode.COAST;
 
-    public static final double kRobotStabilizationToleranceDegrees = 3;
+    public static final double kRobotStabilizationToleranceDegrees = 5;
 
     public static class ControlValues {
       public static class Characterization {
@@ -110,7 +110,7 @@ public final class Constants {
         (4.0 / 1.0) * (10.0 / 1.0) * (66.0 / 18.0); // TODO
 
     @SuppressWarnings("PointlessArithmeticExpression")
-    public static final double kExtensionGearRatio = (10.0 / 1.0);
+    public static final double kExtensionGearRatio = (10.0 / 1.0) * (4.0 / 1.0);
 
     public static final double kExtensionWinchRadiusInches = (3.0 / 4.0) / 2.0;
     public static final double kExtensionWinchRadiusMeters =
@@ -119,7 +119,7 @@ public final class Constants {
     public static final double kExtensionConversionFactor =
         (1 / kExtensionGearRatio) * 2 * Math.PI * kExtensionWinchRadiusMeters;
 
-    public static final double kRotationAbsoluteEncoderOffsetDegrees = -55.723;
+    public static final double kRotationAbsoluteEncoderOffsetDegrees = -32.859;
 
     public static class ControlValues {
       public static class RotationValues {
