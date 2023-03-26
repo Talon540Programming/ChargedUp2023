2e6aea8b4b9a24939489efb44c45a4c4f1765ff2
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-03-25 22:31:31 -0400

    Fix issue with intake not properly setting

--- src/main/java/frc/robot/intake/IntakeIOSparkMax.java
+++ src/main/java/frc/robot/intake/IntakeIOSparkMax.java
@@ -9,52 +9,53 @@ import frc.lib.SparkMaxPeriodicFrameConfig;
 import frc.robot.constants.Constants;
 
 public class IntakeIOSparkMax implements IntakeIO {
-  private final CANSparkMax m_leftMotor;
-  private final CANSparkMax m_rightMotor;
+  private final CANSparkMax m_leader;
+  private final CANSparkMax m_follower;
 
   private final RelativeEncoder m_leftEncoder;
   private final RelativeEncoder m_rightEncoder;
 
   public IntakeIOSparkMax(int leftId, int rightId, double conversionFactor) {
-    m_leftMotor = new CANSparkMax(leftId, CANSparkMaxLowLevel.MotorType.kBrushless);
-    m_rightMotor = new CANSparkMax(rightId, CANSparkMaxLowLevel.MotorType.kBrushless);
+    m_leader = new CANSparkMax(leftId, CANSparkMaxLowLevel.MotorType.kBrushless);
+    m_follower = new CANSparkMax(rightId, CANSparkMaxLowLevel.MotorType.kBrushless);
 
     if (SparkMaxBurnManager.shouldBurnFlash()) {
-      m_leftMotor.restoreFactoryDefaults();
-      m_rightMotor.restoreFactoryDefaults();
+      m_leader.restoreFactoryDefaults();
+      m_follower.restoreFactoryDefaults();
     }
 
-    m_leftMotor.setCANTimeout(SparkMaxBurnManager.kConfigurationStatusTimeoutMs);
-    m_rightMotor.setCANTimeout(SparkMaxBurnManager.kConfigurationStatusTimeoutMs);
+    m_leader.setCANTimeout(SparkMaxBurnManager.kConfigurationStatusTimeoutMs);
+    m_follower.setCANTimeout(SparkMaxBurnManager.kConfigurationStatusTimeoutMs);
 
-    SparkMaxPeriodicFrameConfig.configureLeader(m_leftMotor);
-    SparkMaxPeriodicFrameConfig.configureFollower(m_rightMotor);
+    SparkMaxPeriodicFrameConfig.configureLeader(m_leader);
+    SparkMaxPeriodicFrameConfig.configureFollower(m_follower);
 
-    m_rightMotor.follow(m_leftMotor, true);
+    m_leader.setInverted(true);
+    m_follower.follow(m_leader);
 
-    m_leftMotor.setSmartCurrentLimit(20);
-    m_rightMotor.setSmartCurrentLimit(20);
+    m_leader.setSmartCurrentLimit(20);
+    m_follower.setSmartCurrentLimit(20);
 
-    m_leftMotor.enableVoltageCompensation(12.0);
+    m_leader.enableVoltageCompensation(12.0);
 
-    m_leftEncoder = m_leftMotor.getEncoder();
+    m_leftEncoder = m_leader.getEncoder();
     m_leftEncoder.setMeasurementPeriod(10);
     m_leftEncoder.setAverageDepth(2);
     m_leftEncoder.setPositionConversionFactor(conversionFactor);
     m_leftEncoder.setVelocityConversionFactor(conversionFactor / 60.0);
 
-    m_rightEncoder = m_rightMotor.getEncoder();
+    m_rightEncoder = m_follower.getEncoder();
     m_rightEncoder.setMeasurementPeriod(10);
     m_rightEncoder.setAverageDepth(2);
     m_rightEncoder.setPositionConversionFactor(conversionFactor);
     m_rightEncoder.setVelocityConversionFactor(conversionFactor / 60.0);
 
-    m_leftMotor.setCANTimeout(0);
-    m_rightMotor.setCANTimeout(0);
+    m_leader.setCANTimeout(0);
+    m_follower.setCANTimeout(0);
 
     if (SparkMaxBurnManager.shouldBurnFlash()) {
-      m_leftMotor.burnFlash();
-      m_rightMotor.burnFlash();
+      m_leader.burnFlash();
+      m_follower.burnFlash();
     }
 
     setNeutralMode(Constants.NeutralMode.BRAKE);
@@ -62,10 +63,9 @@ public class IntakeIOSparkMax implements IntakeIO {
 
   @Override
   public void updateInputs(IntakeInputs inputs) {
-    inputs.CurrentAmps =
-        new double[] {m_leftMotor.getOutputCurrent(), m_rightMotor.getOutputCurrent()};
+    inputs.CurrentAmps = new double[] {m_leader.getOutputCurrent(), m_follower.getOutputCurrent()};
     inputs.TemperatureCelsius =
-        new double[] {m_leftMotor.getMotorTemperature(), m_rightMotor.getMotorTemperature()};
+        new double[] {m_leader.getMotorTemperature(), m_follower.getMotorTemperature()};
 
     inputs.LeftVelocityRadPerSecond = m_leftEncoder.getVelocity();
     inputs.RightVelocityRadPerSecond = m_rightEncoder.getVelocity();
@@ -74,12 +74,12 @@ public class IntakeIOSparkMax implements IntakeIO {
   @Override
   public void setVoltage(double voltage) {
     voltage = MathUtil.clamp(voltage, -12, 12);
-    m_leftMotor.setVoltage(voltage);
+    m_leader.setVoltage(voltage);
   }
 
   @Override
   public void setNeutralMode(Constants.NeutralMode mode) {
-    m_leftMotor.setIdleMode(mode.toIdleMode());
-    m_rightMotor.setIdleMode(mode.toIdleMode());
+    m_leader.setIdleMode(mode.toIdleMode());
+    m_follower.setIdleMode(mode.toIdleMode());
   }
 }
