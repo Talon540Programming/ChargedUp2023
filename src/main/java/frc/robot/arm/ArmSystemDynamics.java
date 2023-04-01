28335764c536ce63fc2a70bc96f9845fb24a29f6
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-04-01 04:58:12 -0400

    update MoI values
    thx Aarushi for doing the stuff

--- src/main/java/frc/robot/arm/ArmSystemDynamics.java
+++ src/main/java/frc/robot/arm/ArmSystemDynamics.java
@@ -6,7 +6,7 @@ public class ArmSystemDynamics {
   /**
    * Calculate the Moment of Inertia of the entire arm system based on the length from the pivot
    * point to the beginning of the effector. Formula is estimated using Polynomial regression found
-   * <a href="https://www.desmos.com/calculator/jmuptudz6i">here</a>. Regression follows an R^2
+   * <a href="https://www.desmos.com/calculator/1ok9sw0qvz">here</a>. Regression follows an R^2
    * value of 0.9999.
    *
    * @param pivotToEffectorMeters distance from the pivot to the origin (beginning point) of the
@@ -14,6 +14,6 @@ public class ArmSystemDynamics {
    * @return estimated MoI of the entire Arm System in kilograms meters squared.
    */
   public static double calculateSystemMomentOfInertia(double pivotToEffectorMeters) {
-    return 2.953 * Math.pow(pivotToEffectorMeters, 2) + 2.11558 * pivotToEffectorMeters - 0.10728;
+    return 2.63653 * Math.pow(pivotToEffectorMeters, 2) + 2.29501 * pivotToEffectorMeters - 0.153263;
   }
 }
