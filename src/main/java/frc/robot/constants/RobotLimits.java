cd1a6f0ce920c2674ecee85b5131aad3d1d513e0
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-04-03 03:29:57 -0400

    Update RobotLimits.java

--- src/main/java/frc/robot/constants/RobotLimits.java
+++ src/main/java/frc/robot/constants/RobotLimits.java
@@ -1,5 +1,6 @@
 package frc.robot.constants;
 
+import com.pathplanner.lib.PathConstraints;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
 
@@ -14,6 +15,8 @@ public class RobotLimits {
 
   public static final double kMaxDrivetrainVelocityMetersPerSecond = 4; // TODO
   public static final double kMaxDrivetrainAccelerationMetersPerSecondSquared = 3.5; // TODO
+
+  public static final PathConstraints kTrajectoryConstraints = new PathConstraints(kMaxDrivetrainVelocityMetersPerSecond, kMaxDrivetrainAccelerationMetersPerSecondSquared);
 
   public static final double kMaxArmVelocityRadPerSecond = 3 * Math.PI / 4;
   public static final double kMaxArmAccelerationRadPerSecondSquared = Math.PI / 3;
