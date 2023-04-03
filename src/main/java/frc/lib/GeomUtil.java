4f0d37c6d5e95e06107e87a4b72811f2c8fbabd5
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-04-03 15:07:39 -0400

    Update GeomUtil.java

--- src/main/java/frc/lib/GeomUtil.java
+++ src/main/java/frc/lib/GeomUtil.java
@@ -1,13 +1,10 @@
 package frc.lib;
 
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Transform2d;
-import edu.wpi.first.math.geometry.Translation2d;
 
 public class GeomUtil {
 
   public static Pose2d invertAngle(Pose2d pose) {
-    return pose.transformBy(new Transform2d(new Translation2d(), Rotation2d.fromDegrees(180)));
+    return new Pose2d(pose.getTranslation(), pose.getRotation().unaryMinus());
   }
 }
