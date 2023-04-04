349d1d3ce54d3eee2d7be41b1b97fe6b9d4c5d40
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-04-04 12:35:34 -0400

    end AutoBalance with stabilization

--- src/main/java/frc/robot/groups/AutoBalance.java
+++ src/main/java/frc/robot/groups/AutoBalance.java
@@ -1,21 +1,26 @@
 package frc.robot.groups;
 
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import frc.robot.arm.ArmBase;
 import frc.robot.arm.ArmState;
+import frc.robot.arm.commands.GoToState;
 import frc.robot.arm.commands.GoToSuppliedState;
 import frc.robot.constants.RobotLimits;
 import frc.robot.drivetrain.DriveBase;
 import frc.robot.drivetrain.commands.StabilizeRobot;
 
-public class AutoBalance extends ParallelRaceGroup {
+public class AutoBalance extends ParallelCommandGroup {
     public AutoBalance(DriveBase driveBase, ArmBase armBase) {
         super(
-                new StabilizeRobot(driveBase),
-                new GoToSuppliedState(armBase, () -> new ArmState(
-                        (Math.PI / 2) - driveBase.m_driveInputs.PitchPositionRad,
-                        RobotLimits.kMinArmLengthMeters
-                ))
+                new ParallelRaceGroup(
+                        new StabilizeRobot(driveBase),
+                        new GoToSuppliedState(armBase, () -> new ArmState(
+                                (Math.PI / 2) - driveBase.m_driveInputs.PitchPositionRad,
+                                RobotLimits.kMinArmLengthMeters
+                        ))
+                ),
+                new GoToState(armBase, ArmState.IDLE)
         );
     }
 }
